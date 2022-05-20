34de20593b4c989bcbdb5c47e7c078319353765e
samar-1601 <samar.pp.singh@gmail.com>
2022-05-20 21:54:09 +0530

    All functionality implemented | updating the edited title left

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const leftGrid = document.querySelector(".left-grid");
 const rightGrid= document.querySelector(".right-grid");
 const listItems = document.querySelector(".list-items");
 
+const getTruncatedText = (title)=>{
+    // console.log(title);
+    let truncatedTitle = "";
+    if(title.length > 31)
+    {
+        truncatedTitle = title.slice(0,14) + "..." + title.slice(-14);
+        return truncatedTitle;
+    }
+    return title;
+}
 const addHTMLComponents = (value, id) =>{
 
-    console.log(id);
     const listItem = document.createElement("li");
     const listItemText = document.createElement("span");
     const listItemImage = document.createElement("img");
@@ -13,15 +22,26 @@ const addHTMLComponents = (value, id) =>{
     listItemText.classList.add("list-item-text")
     listItemImage.classList.add("list-item-image")
 
-    listItem.id = id;
-    listItemText.innerText = value.title;
+    listItem.id = "l" + id.toString();
+    listItemText.innerText = getTruncatedText(value.title);
     listItemImage.src = value.previewImage;
+    listItem.style.borderRadius = "5px";
+
+    listItem.addEventListener("click",
+        ()=>toggleRightImage(listItem, id)
+    )
 
     listItem.appendChild(listItemImage)
     listItem.appendChild(listItemText)
     listItems.appendChild(listItem);
-    console.log(listItem)
-    
+    // console.log(listItem)
+    if(id==0)
+        toggleRightImage(listItem,0);
+}
+
+const updateImageDescription = (id, newDesc)=>{
+    const listItem = document.querySelector("#"+"l"+id).querySelector("span");
+    listItem.innerText = newDesc;
 }
 
 const constructRightPane = ()=>{
@@ -30,30 +50,44 @@ const constructRightPane = ()=>{
     
     rightImageDescription.classList.add("right-image-desc");
     rightImage.classList.add("right-image");
-    
+
     rightGrid.appendChild(rightImage);
     rightGrid.appendChild(rightImageDescription);
+
 }
+let prevListItem;
+const toggleRightImage = (listItem, id)=>{
 
-const toggleRightImage = (item)=>{
+    if(prevListItem)
+    {
+        prevListItem.style.backgroundColor = "";
+        prevListItem.style.color = "black";
+    }
+    listItem.style.backgroundColor = "#015ece";
+    listItem.style.color = "white";
 
+    item = itemData[id];
     const rightImageDescription = document.querySelector(".right-image-desc");
     const rightImage = document.querySelector(".right-image");
 
     rightImageDescription.innerHTML = item.title;
     rightImage.src = item.previewImage;
+
+    //updating title functionality
+
+    // rightImageDescription.addEventListener("input", 
+    //     ()=> updateImageDescription(id, rightImageDescription.value)
+    // )
+
+    prevListItem = listItem;
 }
 
 function mainFunc(){
-    console.log(itemData)
+    // console.log(itemData)
+    constructRightPane()
     itemData.forEach((value, index) => {
         addHTMLComponents(value, index)
     });
-    constructRightPane()
-    toggleRightImage(itemData[0]);
-    // hovering functionality
-    
-    
 }
 
 
